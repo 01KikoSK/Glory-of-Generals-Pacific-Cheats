0d88043dd5ac227025e7fc8a4f0fd12ff71a780d
01KikoSK <59251184+01KikoSK@users.noreply.github.com>
2024-04-23 11:50:03 +0200

    Add files via upload

---
+++ Glory Of Generals Pacific Cheat Menu.lua
@@ -0,0 +1,18 @@
+-- Glory of Generals: Pacific cheat script
+
+-- Search for the following values and edit them to the given value
+-- Replace '9999999' with the value you want to edit to
+
+-- Search for 1019;76;874;749;1019 and edit it to 9999999
+gg.clearResults()
+gg.searchNumber('1019;76;874;749;1019', gg.TYPE_FLOAT)
+gg.getResults(100) 
+gg.editAll('9999999', gg.TYPE_FLOAT)
+gg.clearResults()
+
+-- Search for 1128;58;798;766;1128 and edit it to 9999999
+gg.clearResults()
+gg.searchNumber('1128;58;798;766;1128', gg.TYPE_FLOAT)
+gg.getResults(100) 
+gg.editAll('9999999', gg.TYPE_FLOAT)
+gg.clearResults()
